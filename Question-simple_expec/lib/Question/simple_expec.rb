6049e2b9cecc7f4ba24039037fe6f870c56d3c4d
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-10-29 17:18:24 +0000

    Gema creada

---
+++ Question-simple_expec/lib/Question/simple_expec.rb
@@ -0,0 +1,7 @@
+require "Question/simple_expec/version"
+
+module Question
+  module SimpleExpec
+    # Your code goes here...
+  end
+end
---
+++ Question-simple_expec/lib/Question/simple_expec/version.rb
@@ -0,0 +1,5 @@
+module Question
+  module SimpleExpec
+    VERSION = "0.0.1"
+  end
+end
