c836999f4326ab3d801caa01aaeccb8c72ca704e
Gabi <alu0100499377@ull.edu.es>
2014-10-30 21:00:28 +0100

    Comentado las preguntas en formato array

--- Question-simple_expec/lib/Question/base.rb
+++ Question-simple_expec/lib/Question/base.rb
@@ -42,14 +42,45 @@ module Question
 end
 
 if __FILE__ == $0 then #si se utiliza desde un require estas lineas no se ejecutan pero si lo ejecutamos en consola funciona
-  #qq = Question::SimpleExpec.new(text: '2+2 =', right: 4, distractor: [9,3,1])
-  #puts qq.to_html
-  ar = [Question::SimpleExpec.new(text: "¿que devuelve?\n class Xyz\n def pots\n @nice\n end\n end\n xyz = Xyz.new\n p xyz.pots", right: 'nil', distractor: ['#<Xyz:0xa000208>',0,'ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Es valida la definicion?\n hash_raro = {\n [1, 2, 3] => Object.new(),\n Hash.new => :toto\n }\n", right: 'cierto', distractor: ['falso']), Question::SimpleExpec.new(text: "¿cual es la salida?\n class Array\n def say_hi\n HEY\n end\n end\n p[1, bob].say_hi\n", right: 'HEY!', distractor: ['1','bob','ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Cual es el tipo de objeto?\n class Objeto\n end\n", right: 'Un objeto', distractor: ['Una instancia de la clase Class','Una constante','ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Es apropiado que una clase tablero herede de una clase juego?\n", right: 'Cierto', distractor: ['False'])]
+  qq = Question::SimpleExpec.new(text: '2+2 =', right: 4, distractor: [9,3,1])
+  puts qq.to_html
+
+=begin 
+ar = [Question::SimpleExpec.new(text: "¿que devuelve?\n class Xyz\n def pots\n @nice\n end\n end\n xyz = Xyz.new\n p xyz.pots", right: 'nil', distractor: ['#<Xyz:0xa000208>',0,'ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Es valida la definicion?\n hash_raro = {\n [1, 2, 3] => Object.new(),\n Hash.new => :toto\n }\n", right: 'cierto', distractor: ['falso']), Question::SimpleExpec.new(text: "¿cual es la salida?\n class Array\n def say_hi\n HEY\n end\n end\n p[1, bob].say_hi\n", right: 'HEY!', distractor: ['1','bob','ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Cual es el tipo de objeto?\n class Objeto\n end\n", right: 'Un objeto', distractor: ['Una instancia de la clase Class','Una constante','ninguna de las anteriores']), Question::SimpleExpec.new(text: "¿Es apropiado que una clase tablero herede de una clase juego?\n", right: 'Cierto', distractor: ['False'])]
   puts ar[0].to_html
   puts ar[1].to_html
   puts ar[2].to_html
   puts ar[3].to_html
   puts ar[4].to_html
-  
-  
+=end
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
