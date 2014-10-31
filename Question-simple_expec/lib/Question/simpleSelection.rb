3f4ee5acad90e38a655bf11a244a037520874f59
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-10-31 14:37:37 +0000

    Se añade el fichero simpleselection

---
+++ Question-simple_expec/lib/Question/simpleSelection.rb
@@ -0,0 +1,21 @@
+class SimpleSelection
+
+	attr_accessor :enunciado, :op
+
+	def initialize(enunciado, op)
+
+	 @enunciado, @op  = enunciado, op
+
+	end
+
+	def to_s
+
+	cadena ="#{@enunciado}\n"
+
+	@op.each {|m| cadena += "#{m}\n"}
+	cadena
+        end
+
+end
+
+
