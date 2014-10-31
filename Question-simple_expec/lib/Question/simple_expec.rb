6ff75cde0d26a929129a99680881406f0309d057
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2014-10-31 14:40:27 +0000

    Modificamos las rutas de los ficheros

--- Question-simple_expec/lib/Question/simple_expec.rb
+++ Question-simple_expec/lib/Question/simple_expec.rb
@@ -1,7 +1,13 @@
 require 'Question/base'
+require "simple_expec/version.rb"
+require "simple_expec/simpleSelection.rb"
+require "simple_expec/lista.rb"
 
 module Question
   class SimpleExpec
     # Your code goes here...
   end
 end
+
+
+
